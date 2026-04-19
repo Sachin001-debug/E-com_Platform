a16952719a93e14b0fbfac7a846472b5270dcfa7
sachinkharel444 <ukharel111@gmail.com>
2026-04-19 17:13:51 +0545

    minor updates

--- User/my-app/src/pages/Cart/Cart.jsx
+++ User/my-app/src/pages/Cart/Cart.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
-    const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+    const {cartItems,food_list,removeFromCart,getTotalCartAmount,url} = useContext(StoreContext);
 
   const navigate = useNavigate();
 
@@ -32,7 +32,7 @@ const Cart = () => {
              <div>
               <div className="cart-items-tittle cart-items-item">
                 
-                <img src={item.image} alt="" />
+                <img src={url+"/images/"+item.image} alt="" />
                 <p>{item.name}</p>
                 <p>${item.price}</p>
                 <p>{cartItems[item._id]}</p>
